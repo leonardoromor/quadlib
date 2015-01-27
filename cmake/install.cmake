1963d00465063fed7ba2b1da88bbe16ab4872895
EricPascolo <echi.good@gmail.com>
2015-01-27 14:42:40 +0100

    bug fix path

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -2,10 +2,8 @@
 #----------------------------------------------------------------------------
 # INSTALLATION
 
-file(GLOB mathgl_lib ${PROJECT_SOURCE_DIR}/lib/*)
-
-install (TARGETS ${PROJECT_SOURCE_DIR}/libquadrature.release DESTINATION ${CMAKE_INSTALL_PREFIX}/bin)
-install (TARGETS ${PROJECT_SOURCE_DIR}/libquadrature.debug DESTINATION ${CMAKE_INSTALL_PREFIX}/bin)
+install (TARGETS ${PROJECT_SOURCE_DIR}/lib/libquadrature.release DESTINATION ${CMAKE_INSTALL_PREFIX}/bin)
+install (TARGETS ${PROJECT_SOURCE_DIR}/lib/libquadrature.debug DESTINATION ${CMAKE_INSTALL_PREFIX}/bin)
 install (DIRECTORY ${PROJECT_SOURCE_DIR}/include/ DESTINATION ${CMAKE_INSTALL_PREFIX}/include)
 
 #----------------------------------------------------------------------------
